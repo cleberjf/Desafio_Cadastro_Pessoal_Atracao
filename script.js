6d73e986e6b3a71a05f0ed9d0f4649a4c6005b73
cleberjf <87232614+cleberjf@users.noreply.github.com>
2021-07-30 12:03:09 -0300

    Alteração de alerta do submit

---
+++ script.js
@@ -0,0 +1,10 @@
+function submeterForm() { 
+
+  var nome = document.getElementById("name");
+  
+  console.log(nome);
+
+  if (nome.value != "") {
+      alert('Sr ' + nome.value + ', seu cadastro foi encaminhado com sucesso');
+  }
+}
