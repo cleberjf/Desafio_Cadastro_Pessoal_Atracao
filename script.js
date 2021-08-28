c2a8871dfd54d3628b6f7a161c56432579cc7030
cleberjf <cleberjefersonsilva@gmail.com>
2021-08-28 03:19:58 -0300

    formulário

---
+++ script.js
@@ -0,0 +1,11 @@
+function submeterForm() { 
+
+  var nome = document.getElementById("name");
+  
+  console.log(nome);
+
+  if (nome.value != "") {
+      alert('Sr ' + nome.value + ', seu cadastro foi encaminhado com sucesso');
+
+  }
+}
